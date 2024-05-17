744364c268e78e50cb8c0078f11854cabe8fa8c8
hackerskul <hackerskull254@gmail.com>
2024-05-17 05:16:07 -0400

    base model modf

--- models/base_model.py
+++ models/base_model.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 class BaseModel:
     """A base class for all models"""
-    def __init__(self):
+    def __init__(self, *args **kwargs):
+        from models import storage
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
